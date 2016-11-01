41b0ed8dbf16be0b2426e4e395323014f51f6321
pakuyuya <pakuyuya@yahoo.co.jp>
2016-11-01 22:08:00 +0900

    add copy-paste/basics

---
+++ copy-paste/basics/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var elmTx = forId('tx');
+
+elmTx.addEventListener('copy', function(e) {
+    boty('copy', 'Copy', window.getSelection().toString());
+});
+elmTx.addEventListener('paste', function(e) {
+    boty('paste', 'paste', e.clipboardData.getData('Text'));
+});
+
+
+function forId(id) {
+    return document.getElementById(id);
+}
+
+function boty(clazz, caption, text) {
+    var bot = forId('bot');
+    var botCaption = forId('bot-caption');
+    var botSays = forId('bot-says');
+
+    bot.setAttribute('class', clazz);
+    botCaption.innerText = caption;
+    botSays.innerText = text;    
+}
