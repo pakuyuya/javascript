217161f54f40e8719fcb4468e49452738956d3d0
pakuyuya <pakuyuya@yahoo.co.jp>
2019-07-03 23:22:13 +0900

    add timer

---
+++ pixi/02.heiankyo-alian/static/src/js/app/common/Timer.js
@@ -0,0 +1,19 @@
+
+module.exports = class IntervalTimer {
+    constructor (handler, interval) {
+        let self = this
+        self.canceled = false
+
+        let loopFunc = () => {
+            if (!self.canceled) {
+                handler()
+                self.timerid = setTimeout(loopFunc, interval)
+            }
+        }
+        loopFunc()
+    }
+    
+    stop () {
+        this.canceled = true
+    }
+}
