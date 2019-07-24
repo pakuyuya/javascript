a53637e617afb2072c33721a0465e3d8eadd1ec2
pakuyuya <pakuyuya@yahoo.co.jp>
2019-07-25 01:04:37 +0900

    移動速度向上

--- pixi/02.heiankyo-alian/static/src/js/app/common/constants.js
+++ pixi/02.heiankyo-alian/static/src/js/app/common/constants.js
@@ -2,7 +2,7 @@ export default {
     rowBlockSize: 18,
     colBlockSize: 21,
     pixByBlock: 32,
-    pixByStep: 4,
+    pixByStep: 6,
     canvasWidth: 672,
     canvasHeight: 576,
     imageResourceRoot: 'img',
